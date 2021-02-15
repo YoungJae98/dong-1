63cfe75b317802b68725bb0df9db7d6e036ac195
김영재 <68963402+YoungJae98@users.noreply.github.com>
2021-02-15 19:13:17 +0900

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,6 @@
 var mysql = require("mysql");
 var db = mysql.createConnection({
-  host: "18.217.248.102",
-  port: 3306,
+  host: "localhost",
   user: "dong",
   password: "03170317",
   database: "dong",
