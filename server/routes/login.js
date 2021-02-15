cf6e8eb5903c9a5d27b06992307d1c9d35795429
Youngjae Kim <yj122198@naver.com>
2021-02-16 01:50:59 +0900

    admin login

--- server/routes/login.js
+++ server/routes/login.js
@@ -31,49 +31,58 @@ router.post("/login", (req, res) => {
     scriptPath: "./server/",
     args: [req.body.id, req.body.pw],
   };
-  PythonShell.run("sejongAPI.py", options, (err, results) => {
-    if (err) throw err;
-    if (results[0] === "success") {
-      req.session.isLogin = true;
-      req.session.user = results[1];
-      req.session.save(() => {
-        console.log(req.session);
-        db.query(
-          "select * from user where u_id = ?",
-          [results[1]],
-          (logErr, users) => {
-            if (users.length) {
-              res.json({
-                isLogin: results[0],
-                id: results[1],
-                name: results[2],
-                department: results[3],
-                type: users[0].u_auth,
-              });
-            } else {
-              db.query(
-                "insert into user(u_id, u_name, u_department) values(?,?,?)",
-                [results[1], results[2], results[3]],
-                (regErr, regRes) => {
-                  res.json({
-                    isLogin: results[0],
-                    id: results[1],
-                    name: results[2],
-                    department: results[3],
-                    type: 1,
-                  });
-                }
-              );
+  if (req.body.id == "admin" && req.body.pw == "dong1298") {
+    res.json({
+      isLogin: "success",
+      id: "admin",
+      name: "관리자",
+      department: "동아리연합회",
+      type: 2,
+    });
+  } else {
+    PythonShell.run("sejongAPI.py", options, (err, results) => {
+      if (err) throw err;
+      if (results[0] === "success") {
+        req.session.isLogin = true;
+        req.session.user = results[1];
+        req.session.save(() => {
+          db.query(
+            "select * from user where u_id = ?",
+            [results[1]],
+            (logErr, users) => {
+              if (users.length) {
+                res.json({
+                  isLogin: results[0],
+                  id: results[1],
+                  name: results[2],
+                  department: results[3],
+                  type: 1,
+                });
+              } else {
+                db.query(
+                  "insert into user(u_id, u_name, u_department) values(?,?,?)",
+                  [results[1], results[2], results[3]],
+                  (regErr, regRes) => {
+                    res.json({
+                      isLogin: results[0],
+                      id: results[1],
+                      name: results[2],
+                      department: results[3],
+                      type: 1,
+                    });
+                  }
+                );
+              }
             }
-          }
-        );
-      });
-    } else {
-      res.json({
-        isLogin: results[0],
-      });
-    }
-  });
+          );
+        });
+      } else {
+        res.json({
+          isLogin: results[0],
+        });
+      }
+    });
+  }
 });
 
 router.post("/logout", (req, res) => {
