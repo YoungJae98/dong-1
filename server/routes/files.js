fd4c6fcae6a08b1144a861b4bd56adf546e402cc
Youngjae Kim <yj122198@naver.com>
2021-02-16 00:47:09 +0900

    setProxy

--- server/routes/files.js
+++ server/routes/files.js
@@ -2,6 +2,18 @@ var express = require("express");
 var router = express.Router();
 var db = require("../db");
 
-router.get("/", (req, res) => {});
+router.get("/getFiles", (req, res) => {
+  db.query(
+    "select * from files where f_id = ?",
+    [req.body.id],
+    (err, files) => {
+      var data = { files: [] };
+      for (i = 0; i < files.length; i++) {
+        data["files"].push(files[i]);
+      }
+      res.json(data);
+    }
+  );
+});
 
 module.exports = router;
